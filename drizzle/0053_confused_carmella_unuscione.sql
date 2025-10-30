fdcf1e97419212ef2b54b31f90de28beab10bff4
Cem Karaca <cem.karaca@gmail.com>
2025-10-30 02:59:14 -0400

    fixed issue with the migrations where login was impossible

--- drizzle/0053_confused_carmella_unuscione.sql
+++ drizzle/0053_confused_carmella_unuscione.sql
@@ -1,1 +1,1 @@
-ALTER TABLE "audit_logs" DROP CONSTRAINT "audit_logs_server_uuid_mcp_servers_uuid_fk";
+ALTER TABLE "audit_logs" DROP CONSTRAINT IF EXISTS "audit_logs_server_uuid_mcp_servers_uuid_fk";
